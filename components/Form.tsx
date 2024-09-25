ede66587c1154354c9d054d6bc77672bacb85d3d
tobywiedenhoefer <toby.wiedenh@gmail.com>
2024-09-24 22:09:05 -0500

    Updated form component

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,31 +1,44 @@
 import React from "react";
-import { Form as TForm, View } from "tamagui";
+import { Form as TForm, View, XStack } from "tamagui";
 import Button from "./Button";
 
 type FormProps = {
   children?: React.ReactNode;
-  onSubmit: () => void;
-  secondaryButton?: { verbiage: string; action: () => void };
+  primaryButton: { verbiage: string; action: () => void };
+  secondaryButton: { verbiage: string; action: () => void };
 };
 export default function Form(props: FormProps) {
   return (
     <TForm
       gap="$2"
-      borderWidth={1}
-      borderRadius={"$4"}
+      borderRadius={"$8"}
       backgroundColor={"$background"}
       borderBlockColor={"$borderColor"}
-      padding={"$2"}
+      padding={"$4"}
+      shadowOffset={"$0.5"}
+      shadowOpacity={0.4}
+      shadowRadius={"$6"}
     >
       <View marginVertical={"$4"}>{props.children}</View>
-      <TForm.Trigger asChild>
-        <Button variant="primary"></Button>
-      </TForm.Trigger>
-      {props.secondaryButton ? (
-        <Button onPress={() => props.secondaryButton?.action()}>
-          {props.secondaryButton.verbiage}
-        </Button>
-      ) : null}
+      <XStack display="flex">
+        <View width={"40%"}>
+          <Button
+            variant="secondary"
+            onPress={() => props.secondaryButton.action()}
+            marginRight={"$2"}
+            padding={0}
+          >
+            {props.secondaryButton.verbiage}
+          </Button>
+        </View>
+        <View width={"60%"}>
+          <TForm.Trigger asChild width={"revert"}>
+            <Button variant="primary" marginLeft={"$2"} padding={0}>
+              {props.primaryButton.verbiage}
+            </Button>
+          </TForm.Trigger>
+        </View>
+      </XStack>
     </TForm>
   );
 }
