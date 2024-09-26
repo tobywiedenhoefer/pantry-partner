b381a9e8ffb40bc6621e59abccd712e7e2bbde25
tobywiedenhoefer <toby.wiedenh@gmail.com>
2024-09-25 23:35:29 -0500

    Implemented auth context to login

--- app/login.tsx
+++ app/login.tsx
@@ -1,16 +1,39 @@
-import { H1, YStack } from "tamagui";
+import { router } from "expo-router";
+import { H1, View, YStack } from "tamagui";
 
 import Background from "../components/Background";
 import Form from "../components/Form";
+import FormRow from "../components/FormRow";
+import { useAuth } from "../context/AuthContext";
 
 export default function Login() {
+  const { onLogin } = useAuth();
   return (
     <Background variant="primary">
-      <YStack>
-        <Form onSubmit={() => {}}>
-          <H1>Login</H1>
-        </Form>
-      </YStack>
+      <View
+        flex={1}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        marginBottom="$10"
+      >
+        <YStack marginHorizontal="$4">
+          <Form
+            secondaryButton={{
+              verbiage: "Cancel",
+              action: () => router.back(),
+            }}
+            primaryButton={{
+              verbiage: "Submit",
+              action: () => onLogin!("username", "string"),
+            }}
+          >
+            <H1>Login</H1>
+            <FormRow label="username" />
+            <FormRow label="password" />
+          </Form>
+        </YStack>
+      </View>
     </Background>
   );
 }
